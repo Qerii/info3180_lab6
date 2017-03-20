6874e5a5bc3be7f7dfea4455c0bd5db423406911
Topaz Grant <topaz.grant@mymona.uwi.edu>
2017-03-20 15:00:41 +0000

    AngularJS shopping list

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = angular.module("myShoppingList", []); 
+
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.additem = function(){
+        $scope.errortext = "";
+        if (!$scope.addme) {return;}
+        if ($scope.products.indexOf($scope.addme) == -1) {
+            $scope.products.push($scope.addme);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    };
+    $scope.removeitem = function(x){
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    };
+});
